5205752d620cad5e0766fafdd6920a48cf1d14eb
Klaidas Kukoris <klaidas@esoftware.solutions>
2023-08-18 20:36:46 +0300

    Refactor code, add some things

---
+++ src/components/Header/BotNavbar.js
@@ -0,0 +1,9 @@
+import "../../styles/Header/botNavbar.css";
+
+export default function BotNavbar() {
+    return (
+        <div className="bot_nav">
+            
+        </div>
+    )
+}
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,11 +1,13 @@
 import SmallNavbar from "../components/Header/SmallNavbar.js";
 import MidNavbar from "../components/Header/MidNavbar.js";
+import BotNavbar from "../components/Header/BotNavbar.js";
 
 export default function Header() {
     return (
         <div className="header">
             <SmallNavbar />
             <MidNavbar />
+            <BotNavbar />
         </div>
     )
 }
